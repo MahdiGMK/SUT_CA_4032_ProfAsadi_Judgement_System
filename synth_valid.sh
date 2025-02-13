b9840b1d17690d0bf0d295ae8375b5f2e405513c
MahdiGMK <mbahramiand@gmail.com>
2025-02-13 10:16:58 +0330

    synth_valid.sh

---
+++ synth_valid.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+if [[ ! -e $1 || ! -e $2 ]]; then
+  echo 'Usage : ./synth_valid.sh {Circuit} {TestBench}'
+  echo 'e.g. : ./synth_valid.sh HW1/bench.circ tb.v'
+  echo
+  echo 'Synthesis process uses ~/logisim_evolution_workspace/ as main workspace directory'
+  echo 'You can change this behaviour by assigning LOGISIM_WORKSPACE environment variable'
+  exit
+fi
+LOGISIM_WORKSPACE="${LOGISIM_WORKSPACE:-$HOME/logisim_evolution_workspace}"
+echo 'logisim workspace : ' $LOGISIM_WORKSPACE
+./synthesize.sh $1
+CIRC_NAME=$(basename $1)
+SYNTH_DIR=$LOGISIM_WORKSPACE/"$CIRC_NAME".tmp
+echo $2 $SYNTH_DIR
+./validate.sh $2 $SYNTH_DIR
--- validate.sh
+++ validate.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 if [[ ! -e $1 || ! -e "$2/main/verilog" ]]; then
   echo 'Usage : ./validate.sh {TestBenchName} {LogisimCompilationRoot}'
-  echo 'e.g. : ./validate.sh tb.v ~/logisim_evolution_workspace/bench/'
+  echo 'e.g. : ./validate.sh ./HW1/tb0.v ~/logisim_evolution_workspace/bench/'
   exit
 fi
 
