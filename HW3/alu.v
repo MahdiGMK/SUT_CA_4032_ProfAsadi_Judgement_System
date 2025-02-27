4dd3108c3e4f9ed4c1ad6002a0847586ed0aaf77
MahdiGMK <mbahramiand@gmail.com>
2025-02-28 00:41:37 +0330

    hw3

---
+++ HW3/alu.v
@@ -0,0 +1,10 @@
+// add/sub/div/divu/mul/mulu
+// clo (leading ones)
+// clz (leading zeros)
+// low2high
+// low2low
+// hight2low
+//
+module alu ();
+
+endmodule
--- HW3/carry_select_adder.v
+++ HW3/carry_select_adder.v
@@ -1,3 +1,11 @@
-module carry_select_adder ();
+module carry_select_adder #(
+    parameter int N = 32
+) (
+    input [N-1:0] a,
+    input [N-1:0] b,
+    input cin,
+    output [N-1:0] s,
+    output cout
+);
 
 endmodule
